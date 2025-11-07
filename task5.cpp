46194fdaf12805e9abb652957737ad7a47896519
adnanali55 <f2025-0968@bnu.edu.pk>
2025-11-07 15:03:47 +0500

    Add calorie calculation program in task5.cpp

---
+++ task5.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    float cal=3.6;
+    for (int i=5;i<=30;i=i+5) {
+        cout<<"calories burned in" <<  i <<   "minutes ="  <<i*cal<<endl;
+    }
+}
+
